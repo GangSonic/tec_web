926e52e5c6b762a648b5169306f340a0fb7c9d73
Jass <ixcheeljasminhuertaramos@gmail.com>
2025-10-24 17:12:29 -0600

    Ejercicio 3

---
+++ practicas/p11/p11_con_jquery/product_app/app.js
@@ -0,0 +1,37 @@
+// JSON BASE A MOSTRAR EN FORMULARIO
+var baseJSON = {
+    "precio": 0.0,
+    "unidades": 1,
+    "modelo": "XX-000",
+    "marca": "NA",
+    "detalles": "NA",
+    "imagen": "img/default.png"
+  };
+
+function init() {
+    //Convierte el JSON a string para poder mostrarlo
+    //ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
+     
+    var JsonString = JSON.stringify(baseJSON,null,2);
+    document.getElementById("description").value = JsonString;
+
+    // SE LISTAN TODOS LOS PRODUCTOS
+    listarProductos();
+}
+
+$(function(){
+    console.log("JQuery is working");
+
+    $('#search').keyup(function(){
+        let search = $('#search').val();
+       // console.log("Searching for: " + search);
+        $.ajax({
+            url: 'product-search.php',
+            type: 'POST',
+            data: {search: search},
+            success: function(response){
+                console.log(response)
+            }
+        })
+    })
+}); 
