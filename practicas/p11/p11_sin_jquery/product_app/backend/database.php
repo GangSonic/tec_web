d94171de8990fae09a24796f782ee618afa725b9
Jass <ixcheeljasminhuertaramos@gmail.com>
2025-10-24 18:20:12 -0600

    Ejercicio 3: Agregacion de funciones

--- practicas/p11/p11_sin_jquery/product_app/backend/database.php
+++ practicas/p11/p11_sin_jquery/product_app/backend/database.php
@@ -9,7 +9,7 @@
     /**
      * NOTA: si la conexión falló $conexion contendrá false
      **/
-    if(!$conexion) {
-        die('¡Base de datos NO conextada!');
-    }
+   // if(!$conexion) {
+    //    die('¡Base de datos NO conextada!');
+    //}
 ?>
