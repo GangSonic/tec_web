a391a1fff8adc9d6fbd1cd6182b6569191db6826
Jass <ixcheeljasminhuertaramos@gmail.com>
2025-11-30 13:41:18 -0600

    Revert "Arreglo de problemas en a09"
    This reverts commit a212f4ab36460539e23e773a818c9b480899a506.

---
+++ actividades/a09/product_app/backend/Database/DataBase.php
@@ -0,0 +1,32 @@
+<?php
+namespace TECWEB\MYAPI\Database;
+
+abstract class DataBase {
+    protected $conexion;
+    protected $data; 
+
+    public function __construct($db, $user, $pass) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+    
+        /**
+         * NOTA: si la conexión falló $conexion contendrá false
+         **/
+        if(!$this->conexion) {
+            die('¡Base de datos NO conextada!');
+        }
+        $this->data =[]; 
+        /*else {
+            echo 'Base de datos encontrada';
+        }*/
+    }
+
+    public function getData(){
+        return json_encode($this->data, JSON_PRETTY_PRINT); 
+    }
+}
+?>
